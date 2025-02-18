4738814e08c9420f194723a6f3862297999f2fe0
akshita1204 <akshitatyagi04@gmail.com>
2025-02-18 14:31:45 +0530

    commit changes:

--- Day4/script.js
+++ Day4/script.js
@@ -1,14 +1,34 @@
-const sum=(a,b)=>
-{
-    const ans=a+b;
-    return ans;
-}
+//Elimation of the etra variable
+// const sum=(a,b,c)=>
+// {
+//     const ans=a+b;
+//     return ans;
+    
+// }
 
-const Print=(txt)=>
-{
-    console.log("_____________")
-    console.log(txt);
-    console.log("_______________")
-}
+// const Print=(txt)=>
+// {
+//     console.log("_____________")
+//     console.log(txt);
+//     console.log("_______________")
+// }
 
-Print(sum(10,20))
+// Print(sum(10,20))
+
+//callback=> when you pass a function as a parameter (function invocation)
+const sum=(a,b,c)=>
+    {
+        const ans=a+b;
+        //return ans;
+        c(ans)
+        
+    }
+    
+    const Print=(txt)=>
+    {
+        console.log("_____________")
+        console.log(txt);
+        console.log("_______________")
+    }
+    
+    sum(10,20,Print)
