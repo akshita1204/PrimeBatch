2be162614b19fb2911ecaa2c46eb167fffdc0257
akshita1204 <akshitatyagi04@gmail.com>
2025-02-18 14:25:48 +0530

    Callbacks started

---
+++ Day4/script.js
@@ -0,0 +1,14 @@
+const sum=(a,b)=>
+{
+    const ans=a+b;
+    return ans;
+}
+
+const Print=(txt)=>
+{
+    console.log("_____________")
+    console.log(txt);
+    console.log("_______________")
+}
+
+Print(sum(10,20))
