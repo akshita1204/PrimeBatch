83dde4f9f6b202afc4cf127f0b13fa512b0b5556
akshita1204 <akshitatyagi04@gmail.com>
2025-02-20 16:50:40 +0530

    commit changes

---
+++ Day6/FormHandling/script.js
@@ -0,0 +1,18 @@
+const handleTextChange=(e)=>
+{
+    console.log(e)
+    console.log("change: ",e.target.value)
+}
+
+const handleKeydown=(e)=>
+{
+    console.log(e)
+    console.log("key down: ",e.target.value)
+}
+
+const handleKeyup=(e)=>
+{
+    console.log(e)
+    console.log("key up: ",e.target.value)
+}
+        
