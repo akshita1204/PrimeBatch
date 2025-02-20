24b340c1a575dfe35d5b247bdb1db39ff7bdda48
akshita1204 <akshitatyagi04@gmail.com>
2025-02-20 16:31:41 +0530

    Bubbling and capturing

--- Day6/EventListener/script.js
+++ Day6/EventListener/script.js
@@ -10,7 +10,8 @@ data.forEach((ele)=>
 {
    const newCard=document.createElement('div');  //card div (creating a new div inside the parent div)
 
-   //Use of event Listener
+
+   //event listener to change the color of the card (individually)
    newCard.addEventListener("click",()=>
   {
      newCard.style.backgroundColor=getRandomColor();
@@ -19,9 +20,9 @@ data.forEach((ele)=>
    newCard.className="card"   //giving class name to the newcard
 
    newCard.innerHTML=`
-   <h4>${ele.name}</h4>
-   <h6>${ele.email}</h6>
-   <p>${ele.city}</p>
+   <h4 class='text'>${ele.name}</h4>
+   <h6 class='text'>${ele.email}</h6>
+   <p class='text'>${ele.city}</p>
    `;
 
    root.appendChild(newCard)
@@ -49,4 +50,37 @@ const getRandomColor=()=>
 
     return `rgb(${randomR},${randomG},${randomB})`;
 }
+
+
+
+
+
+//event listener to change the color of the text
+const textElements=document.querySelector('.text');
+textElements.addEventListener("click",()=>
+{
+  textElements.style.backgroundColor=getRandomColor();
+})
+
+
+
+//to each the text color of all the texts
+const textElementsall = document.querySelectorAll('.text'); // Select all elements with class 'text'
+textElementsall.forEach(function(ele) {
+  ele.addEventListener("click", () => { 
+    ele.style.backgroundColor = getRandomColor();  // Apply color to each individual element (ele)
+  })
+});
+
+
+//when we change the color of the paragrpah and doesnot want to change the color of the card
+const textElementsall2 = document.querySelectorAll('.text'); // Select all elements with class 'text'
+textElementsall2.forEach(function(ele) {
+  ele.addEventListener("click", (e) => { 
+    e.stopPropagation();   // Prevent event bubbling to parent (card)
+    ele.style.backgroundColor = getRandomColor();  // Apply color to each individual element (ele)
+  })
+});
+
+ 
 
