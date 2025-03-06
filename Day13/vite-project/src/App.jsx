8056944d210a814551f8f57fe289425c62567541
akshita1204 <akshitatyagi04@gmail.com>
2025-03-06 15:44:17 +0530

    react router

--- Day13/vite-project/src/App.jsx
+++ Day13/vite-project/src/App.jsx
@@ -1,30 +1,43 @@
+import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
+import {BrowserRouter,Routes,Route} from 'react-router'
 const App = () => {
-  const { location } = window;
-  const { pathname } = location;
-  if (pathname === "/") {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    )
-  }
-  else if (pathname === "/search") {
-    return (
-      <div>
-        <SearchPage />
-      </div>
-    )
-  }
-  else {
-    return (
-      <div>
-        <h1>Oops Page not found!</h1>
-        <a href="/">Home</a>
-      </div>
-    )
-  }
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/search" element={<SearchPage/>} />
+      <Route path="*" element={<ErrorPage/>}/>
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
+
+  // const { location } = window;
+  // const { pathname } = location;
+  // if (pathname === "/") {
+  //   return (
+  //     <div>
+  //       <HomePage />
+  //     </div>
+  //   )
+  // }
+  // else if (pathname === "/search") {
+  //   return (
+  //     <div>
+  //       <SearchPage />
+  //     </div>
+  //   )
+  // }
+  // else {
+  //   return (
+  //     <div>
+  //       <h1>Oops Page not found!</h1>
+  //       <a href="/">Home</a>
+  //     </div>
+  //   )
+  // }
---
+++ Day13/vite-project/src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage=()=>
+    {
+        return(
+            <div>
+                <h2>Oops Page not found</h2>
+                <a href="/">Home</a>
+            </div>
+        )
+    }
+    export default ErrorPage
