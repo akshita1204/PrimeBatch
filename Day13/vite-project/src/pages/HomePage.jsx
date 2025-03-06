6828381643a269116dfdfe6c7154fb55a39c3b8e
akshita1204 <akshitatyagi04@gmail.com>
2025-03-06 15:25:17 +0530

    Routing

---
+++ Day13/vite-project/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+const HomePage=()=>
+{
+    return(
+        <div>
+            <header>Hello!</header>
+            <main>
+                <h2>Hello from React App!</h2>
+            </main>
+            <foooter>Copyright</foooter>
+        </div>
+    )
+}
+export default HomePage;
---
+++ Day13/vite-project/src/pages/SearchPage.jsx
@@ -0,0 +1,13 @@
+const SearchPage=()=>
+    {
+        return(
+            <div>
+                <header>Logo</header>
+                <main>
+                    <h2>Search here...</h2>
+                </main>
+                <foooter>Copyright</foooter>
+            </div>
+        )
+    }
+    export default SearchPage;
