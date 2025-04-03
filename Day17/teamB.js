a1c4cc53b96eb320ef658b81f66a937fa8420c3f
akshita1204 <akshitatyagi04@gmail.com>
2025-04-03 16:43:45 +0530

    commit changes

--- Day17/teamB.js
+++ Day17/teamB.js
@@ -1,7 +1,10 @@
+let count=0;
 const sum=(...rest)=>
     {
         const ans=rest.reduce((acc,ele)=>
         {
+              count++;  //it will get print as 10 times as we have 10 ele in total in our function call in teamA
+              console.log(count)
               return acc+ele;
         },0)
         return ans;
