2e1915ff07190d109194a6c61eb62e4dcb47b72b
akshita1204 <akshitatyagi04@gmail.com>
2025-04-29 15:21:58 +0530

    Day 21

--- Day21/src/config/db.js
+++ Day21/src/config/db.js
@@ -2,7 +2,7 @@
 const mongoose=require("mongoose")
 const fs=require('fs');
 const data=fs.readFileSync('.env');
-mongoose.connect(process.env.MONGO_DB_URL).then(()=>
+mongoose.connect(process.env.MONGO_DB_URL,{dbName:"PrimeBatch"}).then(()=>
 {
 console.log(data.toString())
 }).catch((err)=>
