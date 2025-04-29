25fe0976c60bf9602b9adeb85e6242138cd383ad
akshita1204 <akshitatyagi04@gmail.com>
2025-04-29 16:01:22 +0530

    Day 23

--- Day21/src/api/v1/products/controllers/getProductsController.js
+++ Day21/src/api/v1/products/controllers/getProductsController.js
@@ -3,13 +3,28 @@ const {productModel}=require("../../../../models/product_schema.js")
 
 const getProductsController=async (req,res)=>
     {
-          const productList=await productModel.find();
+        const {page=1,limit=6}=req.query;
+
+          let mongoosequery=productModel.find();
+
+          mongoosequery=mongoosequery.where('title').regex(/Apple/i); //to search and match, i is for case sensitive
+
+          mongoosequery=mongoosequery.sort("_id")
+          mongoosequery=mongoosequery.skip((page-1)*limit)
+          mongoosequery=mongoosequery.limit(limit)
+
+          const productList=await mongoosequery;
+          const totalproducts=await  productModel.countDocuments();
+          
            console.log("Request received");
             res.send({
             status:"success",
-            message:"Work in done!",
+            message:"Work is done!",
             data:{
                 products:productList,
+                page,
+                limit,
+                total:totalproducts,
             }
         });
     }
