d989a8496f4c27f00a6454be0eaf2b38353bd315
akshita1204 <akshitatyagi04@gmail.com>
2025-05-01 14:34:11 +0530

    Day 24

--- Day23/vite-project/src/pages/SearchPage.jsx
+++ Day23/vite-project/src/pages/SearchPage.jsx
@@ -3,6 +3,16 @@ import { useState } from "react";
 const SearchPage=()=>
 {
     const [searchText,setSearchText]=useState("");
+
+    const getData=async ()=>
+    {
+        const resp=await fetch("http://localhost:2200/api/v1/products",{
+            method:"GET",
+        });
+        const data=await resp.json();
+        console.log("data",data);
+    }
+    getData();
     return <div>
         <div>
             <input type="text" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
@@ -12,6 +22,8 @@ const SearchPage=()=>
             <h1>Products</h1>
             <div></div>
         </div>
-    </div>
+        </div>   
+        
 }
+
 export {SearchPage};
