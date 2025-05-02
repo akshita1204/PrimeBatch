a64f6201479f857adbaeb39ff72fd6aa4408cfa8
akshita1204 <akshitatyagi04@gmail.com>
2025-05-02 15:26:26 +0530

    Day 25 Backend Done

--- Day21/app.js
+++ Day21/app.js
@@ -4,7 +4,8 @@ require("./src/config/db.js");
 const express=require('express')
 const morgan=require('morgan')
 const {productRouter}=require("./src/api/v1/products/routes/product-routes.js")
-const cors=require("cors")
+const cors=require("cors");
+const { authRouter } = require('./src/api/v1/auth/routes/authRoutes.js');
 const PORT=2200;
 
 const app=express();
@@ -19,6 +20,7 @@ app.use(express.json());
 
 app.use(morgan("dev"));
 app.use("/api/v1/products",productRouter);
+app.use("/api/v1/auth",authRouter);
 
 app.listen(PORT,()=>
 {
